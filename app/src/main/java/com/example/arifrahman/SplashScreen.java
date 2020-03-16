84d997459973e5d1e65ac133eda7ea18c77f0a1a
rahman305021 <smrahmanarif@gmail.com>
2020-03-16 07:29:51 +0600

    up commit

---
+++ app/src/main/java/com/example/arifrahman/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.arifrahman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+   private final int SPLASH_TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainActivity = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(mainActivity);
+                finish();
+
+            }
+        }, SPLASH_TIME);
+    }
+}
